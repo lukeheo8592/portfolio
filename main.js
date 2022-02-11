6dd3775d6691ce3957558e508296f341636a1785
lukeheo8592 <71468858+lukeheo8592@users.noreply.github.com>
2022-02-10 22:39:59 -0500

    scroll to section

--- main.js
+++ main.js
@@ -12,6 +12,26 @@ document.addEventListener('scroll', () =>{
     }
 });
 
+// handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+    
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if(link == null){
+        return;
+    }
+    console.log(link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+})
+
+
+
+
+
+
 
 const typeWriter = function(txtEl, words, wait=2500) {
     this.txtEl =txtEl;
